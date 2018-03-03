39966b112660b6c2aef6c61c95113e38ec225958
Zachary Puthoff <zak.puthoff@gmail.com>
2018-03-03 13:12:46 -0600

    Initial javascript & HTML changes

--- js/resolution-up.js
+++ js/resolution-up.js
@@ -4,3 +4,18 @@
 // load them first, and then once the page finishes loading, use javascript
 // to load up the high resolution, larger size pictures; possibly step
 // by step for a cool affect
+
+function imageUpRes() {
+  setTimeout(function() {
+    document.getElementById("splash-img").src = "img/*02.svg";
+  }, 1000);
+  setTimeout(function() {
+    document.getElementById("splash-img").src = "img/*03.svg";
+  }, 1200);
+  setTimeout(function() {
+    document.getElementById("splash-img").src = "img/*.jpg";
+    document.getElementById("project-01-img").src = "img/James Baldwin.jpg";
+    document.getElementById("project-02-img").src = "img/RFK.jpg";
+    document.getElementById("project-03-img").src = "img/Muhammad Ali.jpg";
+  }, 1400);
+}
