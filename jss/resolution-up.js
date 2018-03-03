cae90dce1c5fc16e373e82fa6ac36f3800cf6f80
Zachary Puthoff <zak.puthoff@gmail.com>
2018-03-03 12:00:18 -0600

    Clean up

--- jss/resolution-up.js
+++
@@ -1,6 +0,0 @@
-// gonna try a little javascript in conjunction an image processing script
-// that I found called "primitive"; it makes low-low-low size svg image
-// files out of a jog by using shape ID and placement; the idea is to
-// load them first, and then once the page finishes loading, use javascript
-// to load up the high resolution, larger size pictures; possibly step
-// by step for a cool affect
